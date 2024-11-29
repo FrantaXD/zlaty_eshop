ad88c3206d59b463dab9ad6103e6bb04aa991bc0
NaRt228 <zakarpatskimulti@gmail.com>
2024-11-29 10:09:31 +0100

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
-import Footer from "@/components/Footer";
+import "./domu.css";
 import Image from "next/image";
-
+import IMG from "../utils/images/image1.png";
+import { Slider } from "@/components/Slider/Slider";
 export default function Home() {
   return (
-    <div>
-      
-    
-    </div>
+    <main>
+      <div className="first-vi">
+        <section className="section-vyroba">
+          <h1 className="h1-vyroba">Výroba autorských šperků</h1>
+          <p className="h1-p-vyroba">
+            Lorem ipsum nevim nevimLorem ipsum nevim nevimLorem ipsum nevim
+            nevimLorem ipsum nevim nevim
+          </p>
+          <button className="button-vice">Více</button>
+        </section>
+        <section className="img-container">
+        <div className="transparent-object"></div>
+          <div className="img-ring">
+            <Image src={IMG} alt="ring" layout="fill" objectFit="cover" />
+          </div>
+        </section>
+      </div>
+      <div>
+        <Slider/>
+      </div>
+      <div>
+        <section className="imgs-container">
+          <div className="fir">
+          <img src="" alt="work3" id="work3"/>
+          </div>
+          <div className="sco">
+            <img src="" alt="work" id="work"/>
+            <img src="" alt="work2" id="work2"/>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
