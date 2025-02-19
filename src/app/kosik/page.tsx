a5d12f897e87ad10d5180c605acae1b239a77eef
rizzer18 <romanroskanuk74@gmail.com>
2025-02-19 13:40:38 +0100

    Some_Change

--- src/app/kosik/page.tsx
+++ src/app/kosik/page.tsx
@@ -13,7 +13,7 @@ export default function Cart() {
 
     useEffect(() => {
         async function Get_Data() {
-            // await post_product({ productId: 2, quantity: 1}).then(e => alert(e?.message));
+             // await post_product({ productId: 2, quantity: 1}).then(e => alert(e?.message));
             const f = await get_products_cart().then(e => e)
             setCarItems(f);
             if (f) setFullPrice(countPrice(f));
@@ -29,12 +29,12 @@ export default function Cart() {
         return price;
     }
     return (
-        <main className=" text-white  flex-1 mt-[100px]">
-            <h1></h1>
-            <div className="display flex justify-center gap-[400px]">
+        <main className=" text-white  flex-1 mt-[60px]">
+           <div className="absolute right-[50%] translate-x-[50%]"><h1 className="absoute mx-auto font-bold text-[64px] text-white">Košík</h1></div> 
+            <div className="display flex justify-between  w-[1100px] m-auto mt-[160px] ">
                 <section className="w-[660px]">{cartItems && cartItems.flatMap((e, index) =>
                     Array.from({ length: e.quantity }, (_, i) => (
-                        <div key={`${index}-${i}`} className="flex gap-6 w-[660px]  mt-5 mb-5" >
+                        <div key={`${index}-${i}`} className="flex gap-6 w-[660px]  mb-12" >
                             <div className="relative w-[239px] h-[239px] overflow-hidden">
                                 <Image
                                     src={e.mediaUrls[0]}
@@ -50,14 +50,14 @@ export default function Cart() {
                                 <p className="text-[35px]">{e.price}</p>
                                 </div>
                                 <div className="flex justify-end">
-                                    <button ><u>Odebrat</u></button>
+                                    <button className="text-[30px] text-zinc-100 text-opacity-50"><u>Odebrat</u></button>
                                 </div>
                             </div>
                         </div>
                     ))
                 )}
                 </section>
-                <section><h2 className="text-[44px]"><b>Celkem: {fullPrice}</b></h2><div className="text-[30px]">Celkem bez DPH: {fullPrice && (fullPrice - (fullPrice / 100 * 21)).toString().substring(0, 5)}</div></section>
+                <section className="text-end flex flex-col gap-[10px] "><h2 className="text-[44px]"><b>Celkem: {fullPrice}</b></h2><div className="text-[30px]">Celkem bez DPH: {fullPrice && (fullPrice - (fullPrice / 100 * 21)).toString().substring(0, 5)}</div> <button className="py-[10px] px-[25px] w-auto self-start bg-orange-100 text-3xl text-black font-bold mt-[20px] ml-auto">Pokračovat</button></section>
             </div>
         </main>
     );
