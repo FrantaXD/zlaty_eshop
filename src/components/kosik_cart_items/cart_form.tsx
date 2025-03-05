ae7e0862ef34914d05d5e0c4913ccf220db13fc8
rizzer18 <romanroskanuk74@gmail.com>
2025-03-05 23:43:59 +0100

    Night_commit

--- src/components/kosik_cart_items/cart_form.tsx
+++ src/components/kosik_cart_items/cart_form.tsx
@@ -1,11 +1,25 @@
-import { Formik } from "formik"
-import { object, number, string } from "yup"
+import { ErrorMessage, Form, Formik, Field } from "formik";
+import * as Yup from "yup";
 
-export function Cart_form(){
-
-    return(
-        <div>
-
-        </div>
-    )
+export function Cart_form() {
+  const validate = Yup.object({
+    name: Yup.string(),
+  });
+  return (
+    <Formik
+      initialValues={{ name: "" }}
+      validationSchema={validate}
+      onSubmit={(values) => {}}
+    >
+      {({ values }) => (
+        <Form>
+          <div>
+            <label htmlFor="name">ss</label>
+            <Field name="name" id="name" />
+            <ErrorMessage name="name" component="div" />
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 }
