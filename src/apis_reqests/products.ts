acefd81c083ba09dc02f3e64e0e23e1bc0f80772
NaRt228 <zakarpatskimulti@gmail.com>
2025-04-23 12:05:08 +0200

    Actual Version 23-04

--- src/apis_reqests/products.ts
+++ src/apis_reqests/products.ts
@@ -1,7 +1,7 @@
 import { Product_cart } from "@/interface/product_cart";
 import { product_curt_post_Interface, responde_cart } from "../interface/product_response";
 import axios from "axios";
-//
+
 const reqest = axios.create({baseURL: "https://apigolde-shop-production-5431.up.railway.app/", headers: {}, withCredentials: true});
 
 
