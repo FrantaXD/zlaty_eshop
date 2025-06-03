d8c57eecf1c948b92ac4e95b0d7a9ecb15fa0ac9
rizzer18 <romanroskanuk74@gmail.com>
2025-06-03 16:26:29 +0200

    fontChange:))))

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} antialiased overflow-x-hidden`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} overflow-x-hidden subpixel-antialiased font-light`}>
         <CartProvider>
           <LayoutWrapper>{children}</LayoutWrapper>
         </CartProvider>
